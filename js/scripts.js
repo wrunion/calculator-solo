c0a17e7ad706a8d94d749af529fb7eb6e391e584
Winter Runion <winterrunion@gmail.com>
2020-01-24 12:34:23 -0800

    manually merge scripts files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,110 +1,61 @@
-//Business Logic
+//Business logic
 
-const add = function(num1, num2) {
-  return num1 + num2;
+let arrayToCalculate = [];
+
+  //NTS for evaluate(): first check that there are at least 2 numbers w/an operator between them arrayToCalculate, or the evaluate function WON'T work -
+  //OR validate that data as it's pressed, and display an error message if user is pressing things out of order
+const evaluate = function(arr) {
+  let stringToEvaluate = arr.join("");
+  console.log(stringToEvaluate);
+  return eval(stringToEvaluate);
 }
 
-const subtract = function(num1, num2) {
-  return num1 - num2;
-}
-const multiply = function(num1, num2) {
-  return num1 * num2;
-}
-const divide = function(num1, num2) {
-  return num1 / num2;
-}
-
-const remainder = function(num1, num2) {
-  return num1 % num2;
-}
-
-//Push that value into an array
-let arrToCalculate = [];
-let stringToCalculate = 0;
-
-
-// UI Logic 
+//UI logic (jQuery)
 $(document).ready(function() {
 
-  $("#clear").click(function() {
-    //alert("you have clicked clear");
-    location.reload(true);
-    //alert("the page has reloaded");
+  //Evaluates the result string when the equals sign is pressed
+  $("#calculate").on("click", function() {
+    event.preventDefault();
+    alert(evaluate(arrayToCalculate));
+    arrayToCalculate = [];
   });
 
-  $("#button1").click(function() {
+  //Push value of each button up to arrayToCalculate as they are pressed
+  //NTS: Go back and make separate (jQuery?) functions for 'clear' and 'backspace.'
+  $(".numbers-and-operators").on("click", function() {
     event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
+    let buttonVal = $(this).val();
+    console.log(buttonVal);
 
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
+    if (buttonVal === "clear") {
+      location.reload();
+    } else if (buttonVal === "backspace") {
+      arrayToCalculate.pop();
+      //also take one off the display - figure that out later
+    } else {
+      arrayToCalculate.push(buttonVal);
+      console.log(arrayToCalculate);
+    }
+
+    // $("#submit").on("click", function() {
+    //   event.preventDefault();
+
+    //   let stringToCalculate = arrayToCalculate.join(""); 
+    //   let result = eval(stringToCalculate);
+    //   console.log(result);
+    //   return result;
+    // });
+
+      // $("#calculate").on("click", function evaluate() {
+      //   event.preventDefault();
+      //   let stringToEvaluate = arrayToCalculate.join("");
+      //   let result = eval(stringToEvaluate);
+      //   console.log(result);
+      //   $("#input").append(result);
+      //   return result;
+      // });
+      
+
   });
-
-  $("#add").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });
-
-  $("#subtract").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });  
-  
-  $("#multiply").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });  
-  
-  $("#divide").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });  
-  
-  $("#remainder").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });
-
-  $("#2").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });
-
-  $("#3").click(function() {
-    event.preventDefault();
-    arrToCalculate.push($(this).val());
-    console.log(arrToCalculate);
-
-    stringToCalculate += $(this).val();
-    console.log(stringToCalculate);
-  });
-
-  
-
 
 });
