6b7453df742f82ad3daad90742fc219f06e48d9e
Winter Runion <winterrunion@gmail.com>
2020-01-24 12:31:50 -0800

    Add WORKING evaluate function to calculate  results of user input.

--- js/scripts3.js
+++ js/scripts3.js
@@ -2,37 +2,60 @@
 
 let arrayToCalculate = [];
 
+  //NTS for evaluate(): first check that there are at least 2 numbers w/an operator between them arrayToCalculate, or the evaluate function WON'T work -
+  //OR validate that data as it's pressed, and display an error message if user is pressing things out of order
+const evaluate = function(arr) {
+  let stringToEvaluate = arr.join("");
+  console.log(stringToEvaluate);
+  return eval(stringToEvaluate);
+}
 
 //UI logic (jQuery)
 $(document).ready(function() {
 
+  //Evaluates the result string when the equals sign is pressed
+  $("#calculate").on("click", function() {
+    event.preventDefault();
+    alert(evaluate(arrayToCalculate));
+    arrayToCalculate = [];
+  });
+
+  //Push value of each button up to arrayToCalculate as they are pressed
+  //NTS: Go back and make separate (jQuery?) functions for 'clear' and 'backspace.'
   $(".numbers-and-operators").on("click", function() {
     event.preventDefault();
-    //console.log('jquery is working for buttons');
     let buttonVal = $(this).val();
     console.log(buttonVal);
 
+    if (buttonVal === "clear") {
+      location.reload();
+    } else if (buttonVal === "backspace") {
+      arrayToCalculate.pop();
+      //also take one off the display - figure that out later
+    } else {
+      arrayToCalculate.push(buttonVal);
+      console.log(arrayToCalculate);
+    }
 
+    // $("#submit").on("click", function() {
+    //   event.preventDefault();
+
+    //   let stringToCalculate = arrayToCalculate.join(""); 
+    //   let result = eval(stringToCalculate);
+    //   console.log(result);
+    //   return result;
+    // });
+
+      // $("#calculate").on("click", function evaluate() {
+      //   event.preventDefault();
+      //   let stringToEvaluate = arrayToCalculate.join("");
+      //   let result = eval(stringToEvaluate);
+      //   console.log(result);
+      //   $("#input").append(result);
+      //   return result;
+      // });
+      
 
   });
-  //add a class to all the integer and operator buttons
-  
-  //target and call that class whenever it's clicked
-  
-  //push the value of that button up to the answer string
-
-  //when "=" (submit button) is pressed, evaluate the answer string with eval()
-
-  //return the result to the user (in a div??? in the input box???)
-
-
-  //OR
-
-  //When any button is pressed:
-
-  // IF it is the "=", run calculate;
-  // IF it is "clear", location.reload();
-  // IF it is backspace, .pop() the last element off the array
-
 
 });
